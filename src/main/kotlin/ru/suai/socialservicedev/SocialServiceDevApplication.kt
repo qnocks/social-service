0032aaed31ff0b131b99df29800996d3ac1e5f17
qnocks <roma.ost2012@mail.ru>
2021-12-19 19:00:18 +0300

    Initial commit

---
+++ src/main/kotlin/ru/suai/socialservicedev/SocialServiceDevApplication.kt
@@ -0,0 +1,11 @@
+package ru.suai.socialservicedev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialServiceDevApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocialServiceDevApplication>(*args)
+}
